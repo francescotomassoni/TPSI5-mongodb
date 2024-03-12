ee02b5222d039304e5e5a7d9dfd0a31517d9e9e1
franz-86 <100193463+franz-86@users.noreply.github.com>
2024-03-12 21:37:55 +0100

    dichiarazioni funzioni db

---
+++ db.js
@@ -0,0 +1,28 @@
+import {MongoClient, ObjectId} from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export async function connectToDB(){
+    
+}
+
+export async function getAll(){
+    
+}
+
+export async function getOne(){
+    
+}
+
+export async function createOne(){
+
+}
+
+export async function deleteOne(){
+
+}
+
+export async function updateOne(){
+
+}
